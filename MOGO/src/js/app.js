48688d51f518d3d5294b69e1a1fbfb3dfac794fd
Dinotosu <valeraleb@gmail.com>
2022-11-09 01:11:03 +0200

    add mogo project

--- MOGO/src/js/app.js
+++ MOGO/src/js/app.js
@@ -1,7 +1,19 @@
 import "../scss/index.scss";
-import '../scss/animation.scss';
+import "../scss/animation.scss";
 
 /* Your JS Code goes here */
 
 /* Demo JS */
 import "./demo.js";
+
+
+new Swiper(".swiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
